9d722e68b883d9e5d0fcf6d03a3b6a5ddd6e4565
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 <a1793138@student.adelaide.edu.au>
2022-09-01 14:04:29 +0930

    li

--- Wagon.cpp
+++ Wagon.cpp
@@ -15,20 +15,15 @@ Wagon::~Wagon()
 
 bool Wagon:: addCapybara(Capybara newCapy)
 {
-    if(index == 3) {
+    if(index >= 3) {
         return false;
-    }
-    // int length =sizeof(newCapy)/sizeof(Capybara);
-    // if (length ==number)
-    // {
-    //     return 1;
-    // }else{
-    //     return 0;
-    // }
-    animals[index] = newCapy;
-    index++;
+    }else {
+        animals[index] = newCapy;
+        index++;
 
-    return true;
+        return true;
+    }
+
 
 }
 void Wagon:: emptyWagon()
